ad7a363ae4c8565442282f03e28ca08dcff23f64
Thibault Berrdo <thibault.berrido@perfectogroupe.fr>
2024-01-30 14:54:07 +0000

    adding link to react learn

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.scss';
 import ComponentOne from './components/One';
 import { Parent as ComponentTwo } from './components/Two';
 
+// generic reference : https://react.dev/learn/thinking-in-react
+
 const App = () => (
 	<>
 		<ComponentOne />
